359e220cef47f8ef920842a326c09293d7577411
NoahbLapp <noahb.lapp@gmail.com>
2024-10-28 11:58:19 -0400

    Done

--- src/App.java
+++ src/App.java
@@ -26,7 +26,36 @@ public class App {
 	}
 	
 	public static boolean isMagic(int[][] array){
-		return false; //placeholder
+		if(array[0].length == array.length){
+			boolean isEqual = true;
+			int sum = 0;
+			for(int i = 0; i < array[0].length; i++){
+				sum += array[0][i];
+			}
+			for(int row = 1; row < array.length; row++){
+				int rowSum = 0;
+				for(int col = 0; col < array[0].length; col++){
+					rowSum += array[row][col];
+				}
+				if(rowSum != sum){
+					isEqual = false;
+					break;
+				}
+			}
+			for(int col = 0; col < array[0].length; col++){
+				int colSum = 0;
+				for(int row = 0; row < array.length; row++){
+					colSum += array[row][col];
+				}
+				if(colSum != sum){
+					isEqual = false;
+					break;
+				}
+			}
+			return isEqual;
+		}else{
+			return false;
+		}
 	}
 	
 }
